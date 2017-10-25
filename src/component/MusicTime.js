d5ac9feb7b614b7690e6245a1b6ca7017ed0ab2d
chenjm <chenjianming@gezhigene.com>
2017-10-25 21:27:19 +0800

    新打包

--- src/component/MusicTime.js
+++ src/component/MusicTime.js
@@ -12,18 +12,15 @@ class MusicTime extends Component {     //播放时间及进度
         if(seconds < 10){
             seconds = '0'+seconds;
         }
-        return minutes+':'+seconds ;
+        return minutes+':'+seconds;
     }
     render(){
         const {currentTime, progress, timeChange, proChange, currentTotalTime} = this.props;
         return (
             <div className="timeline">
-               <span>{this.converTime(currentTime)}</span>
-             
+               <span>{this.converTime(currentTime)}</span>             
                     <div className="prodrag">
                         <Slider max={2000} value={progress * 2000} onChange={timeChange} onAfterChange={proChange} tipFormatter={false}/>
-                    </div>
-               
                <span>{this.converTime(currentTotalTime) }</span>                
             </div>
         )
